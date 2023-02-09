1bf4e637985e8e6b00c5764602f35fad36b6df05
IE1042 <113075218+IE1042@users.noreply.github.com>
2023-02-09 01:13:06 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,5 @@
+var num1 = 15;
+var num2 = 10;
+var num3 = 5;
+var result = num1 * num2 * num3;
+console.log(result);
---
+++ swapValues.js
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 20;
+
+var temp = a;
+a = b;
+b = temp;
+
+console.log("new value of a:", a);
+console.log("new value of b:", b);
